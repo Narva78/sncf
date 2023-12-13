8239898ce36a3ae3f0a00493317b3c4cfa630336
CYRILLE <971cyohann@gmail.com>
2023-12-13 05:13:38 -0400

    html page connexion

--- views/co.php
+++ views/co.php
@@ -2,17 +2,10 @@
 
 
 	<title>Page de connexion</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
 
 <style>
-.card {
-	position: absolute;
-	top: 40%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
+
 </style>
 
 
@@ -20,28 +13,24 @@
 
 
 
-<body style="background-image: url('image/arrierePlan.jpg'); background-size: cover;">
-	<div class ="card container" style = "width: 28rem">
-		<div class="mt-5 text-center">
-			<h2 class="mb-4">Page de Connexion Gestion d'Ipad </h2>
-			<i class="fas fa-star fa-spin"></i>
+<body>
+<section class="card">
+        <div class="card-container">
+            <h2>Gestion Ipad</h2>
+            <form action="index.php?uc=connexion&action=valideConnexion" method="post">
+                <div class="card-login">
+                    <label for="login"></label>
+                    <input type="login" name="login" id="login" placeholder="Username">
+                </div>
+                <div class="card-pass">
+                    <label for="mdp"></label>
+                    <input type="password" name="mdp" id="mdp" placeholder="password">
+                </div>
 
-			<br><br>
-			<!-- Formulaire de connexion à l'aide de bootstrap -->
-			<form method="POST" action="index.php?uc=connexion&action=valideConnexion">
-				<div class="form-group">
-					<label for="login"></label>
-					<input type="login" class="form-control" id="login" placeholder="Entrez le Cp" name="login">
-				</div>
-				<div class="form-group">
-					<label for="mdp"></label>
-					<input type="password" class="form-control" id="mdp" placeholder="Enter password" name="mdp">
-				</div>
-				<br>
-				<input type="submit" class="btn btn-primary" value="Se connecter" name="valider">
-				<br><br>
-			</form>
-		</div>
-	</div>
+                <input type="submit" value="Login" name="Validez">
+            </form>
+        </div>
+
+    </section>
 </body>
 
