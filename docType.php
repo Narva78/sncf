0d1d4c3c6da769f8901adfe39dfd4a22628b1a79
CYRILLE <971cyohann@gmail.com>
2023-12-13 07:52:23 -0400

    modif 2

--- docType.php
+++ docType.php
@@ -8,25 +8,4 @@
     <link rel="stylesheet" href="style/style.css">
 
     <!-- lié au bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
-	<!-- jQuery -->
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<script type="text/javascript" src="../js/jquery-3.6.4.min.js"></script>
-
-	<!-- jQuery UI -->
-	<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.1/themes/smoothness/jquery-ui.css">
-	<script type="text/javascript" src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
-	<link rel="stylesheet" href="../jquery/jquery-ui.css"/>
-	<script type="text/javascript" src="../jquery/jquery-ui.min.js"></script>
-
-	 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	
 </head>
