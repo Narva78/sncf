b087798fb90f3f58b13ce53edfede3d6c87ed338
CYRILLE <971cyohann@gmail.com>
2023-12-13 09:34:53 -0400

    modif message d'erreur

--- views/co.php
+++ views/co.php
@@ -7,7 +7,7 @@
     <section class="card">
         <div class="card__container">
             <h2>Gestion Ipad</h2>
-            <form method="post" action="index.php?uc=connexion&action=valideConnexion">
+            <form method="post" action="index.php?uc=connexion&action=valideConnexion" id="loginForm">
                     <div>
 					<label for="login"></label>
                     <input type="login" class="card__container__login"  name="login" id="login" placeholder=" Username">
@@ -22,3 +22,18 @@
         </div>
 
     </section>
+
+</body>
+<script>
+	document.getElementById('loginForm').addEventListener('submit', function(event) {
+    const login = document.getElementById('login').value;
+    const password = document.getElementById('mdp').value;
+
+    if (login.trim() === 'null' || password.trim() === '') {
+      event.preventDefault(); // Empêche l'envoi du formulaire
+
+      // Affiche une boîte de dialogue (popup) pour l'erreur
+      alert('Veuillez remplir tous les champs');
+    }
+  });
+</script>
